8cf09b605edc9a24f49ae648668f65a12ed6d4ce
w84v2rhsq4 <yter-manvelyants@aligntech.com>
2019-02-11 12:44:24 +0300

    fixed routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { RouteType, getComponentByRoute } from "../../routes";
+import { routes, getComponentByRoute } from "../../routes";
 import Navigation from "../Navigation";
 import Layout from "../Layout";
 
 const App = () => (
   <Router>
     <Layout
-      header={<Navigation items={[RouteType.FEED, RouteType.LOVED]} />}
+      header={<Navigation items={[routes.FEED, routes.LOVED]} />}
       content={
         <>
-          {[RouteType.FEED, RouteType.LOVED, RouteType.SINGLE_IMAGE].map(
-            item => (
-              <Route
-                key={item}
-                path={item}
-                exact
-                component={getComponentByRoute(item)}
-              />
-            )
-          )}
+          {[routes.FEED, routes.LOVED, routes.SINGLE_IMAGE].map(item => (
+            <Route
+              key={item}
+              path={item}
+              exact
+              component={getComponentByRoute(item)}
+            />
+          ))}
         </>
       }
     />
--- src/routes.ts
+++ src/routes.ts
@@ -2,19 +2,23 @@ import FeedContainer from "./containers/FeedContainer";
 import LovedFeedContainer from "./containers/LovedFeedContainer";
 import ImageContainer from "./containers/ImageContainer";
 
-export enum RouteType {
-  FEED = "/",
-  LOVED = "/loved",
-  SINGLE_IMAGE = "/image/:id"
-}
+const FEED = window.location.pathname + "/";
+const LOVED = window.location.pathname + "/loved";
+const SINGLE_IMAGE = window.location.pathname + "/image/:id";
+export const routes = {
+  FEED,
+  LOVED,
+  SINGLE_IMAGE
+};
+export type RouteType = string;
 
 export const getComponentByRoute = (route: RouteType) => {
   switch (route) {
-    case RouteType.FEED:
+    case routes.FEED:
       return FeedContainer;
-    case RouteType.LOVED:
+    case routes.LOVED:
       return LovedFeedContainer;
-    case RouteType.SINGLE_IMAGE:
+    case routes.SINGLE_IMAGE:
       return ImageContainer;
     default:
       throw new Error("undefined route");
@@ -23,9 +27,9 @@ export const getComponentByRoute = (route: RouteType) => {
 
 export const getNavTitleByRoute = (route: RouteType) => {
   switch (route) {
-    case RouteType.FEED:
+    case routes.FEED:
       return "/";
-    case RouteType.LOVED:
+    case routes.LOVED:
       return "<3";
     default:
       throw new Error("undefined nav item");
